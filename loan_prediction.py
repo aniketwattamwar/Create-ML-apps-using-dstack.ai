0f35be28d21eea007f6dac6d89526d6739cbbef1
aniketwattamwar <34825918+aniketwattamwar@users.noreply.github.com>
2020-12-31 17:35:27 +0530

    new tab added

--- loan_prediction.py
+++ loan_prediction.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import pandas as pd 
 import numpy as np 
+import dstack.controls as ctrl
 
 @ds.cache()
 def get_data():
@@ -23,11 +24,11 @@ def bar_handler():
     return px.bar(df, x="Gender", y="Loan_Amount_Term", color="Education", barmode="group")
 
 train_data = get_data()
-# print(train_data.columns)
 y = train_data.iloc[:,-1]
 
 train_data = train_data.drop(['Loan_ID','Loan_Status'],axis=1)
 test_data = get_testdata()
+ids = test_data.iloc[:,0]
 test_data = test_data.drop(['Loan_ID'],axis=1)
 
 def encoding(data):
@@ -49,20 +50,33 @@ from sklearn.ensemble import RandomForestClassifier
 random = RandomForestClassifier()
 random.fit(train,y)
 #When running it normmaly without pushing it to dstack it gave the output
-y_pred = random.predict(test)
+# y_pred = random.predict(test)
 
 ds.push("Random_Forest", random, "Random Forest Loan Prediction")
 
 #The below two lines when uncommented gave the error- Comment the line 51 if below two are to be used
-# model = ds.pull('/dstack/Random_Forest')
-# y_pred = model.predict(test)
-print('predictions')
-print(y_pred)
+model = ds.pull('/dstack/Random_Forest')
+
+# print('predictions')
+# print(y_pred)
+values = ctrl.ComboBox(data=['Predict'], label="Predictions",require_apply=True)
+
+def get_predicted(values: ctrl.ComboBox):
+    y_pred = model.predict(test)
+    y_pred = pd.DataFrame(y_pred)
+    y_pred = y_pred.rename(columns={0:'Prediction'})
+    y_pred['ID'] = ids
+    
+    return y_pred
+
  
 
+p_app = ds.app(get_predicted, values = values)
+
 frame = ds.frame("Loan_Prediction")
+frame.add(p_app, params={"Predicted": ds.tab()})
 frame.add(ds.app(scatter_handler), params={"Visualize": ds.tab()})
-frame.add(ds.app(bar_handler), params={"Predict": ds.tab()})
+frame.add(ds.app(bar_handler), params={"Bar Plot": ds.tab()})
 
 url = frame.push()
 print(url)
